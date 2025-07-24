099d86f16b032f76eea40a5bd495aa9ce10a79cd
tienthuan19 <lebuithuan@gmail.com>
2025-07-24 23:07:12 +0700

    feat(auth): initialize auth route, controller, service

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// >>>>>>>>>>>>> Middleware setup <<<<<<<<<<<<<<
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// >>>>>>>>>>>>>>>>>> Routes <<<<<<<<<<<<<<<<
+require("./app/route")(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
